d67c6a7121267b78c1ab47dc6f39c89fde4f3cec
Kurochi51 <andrei5125@gmail.com>
2023-05-29 00:58:40 +0300

    Updater testing

--- src/Ryujinx/Modules/Updater/Updater.cs
+++ src/Ryujinx/Modules/Updater/Updater.cs
@@ -569,30 +569,21 @@ namespace Ryujinx.Modules
             if (Running)
             {
                 var oldFiles = Directory.EnumerateFiles(HomeDir, "*", SearchOption.TopDirectoryOnly).ToList();
-                foreach (var o in oldFiles)
-                {
-                    Console.WriteLine((Path.GetFileName(o) + " old base dir file."));
-                }
                 var newFiles = Directory.EnumerateFiles(UpdatePublishDir, "*", SearchOption.TopDirectoryOnly).ToList();
-                foreach (var n in newFiles)
-                {
-                    Console.WriteLine((Path.GetFileName(n) + " new update dir file."));
-                }
 
                 //Compare the loose files in base directory against the loose files from the incoming update, and store foreign ones in a user list
                 var UserFiles = newFiles.Except(oldFiles).ToList();
-                List<string> userFiles = new List<string>();
-                foreach (var u in UserFiles)
+                /*List<string> userFiles = new List<string>();
+                foreach (var f in UserFiles)
                 {
-                    userFiles.Add(Path.GetFileName(u));
-                    Console.WriteLine(u + " user file in base dir.");
-                }
+                    userFiles.Add(Path.GetFileName(f));
+                }*/
 
                 //Remove user files from the paths in files
-                foreach (var userFile in userFiles)
+                foreach (var userFile in UserFiles)
                 {
-                    files = files.Where(u => !u.Contains(userFile)).ToList();
-                    Console.WriteLine(userFile + " is being tested against.");
+                    files = files.Where(u => !u.Contains(Path.GetFileName(userFile))).ToList();
+                    //Console.WriteLine(userFile + " is being tested against.");
                 }
             }
             if (OperatingSystem.IsWindows())
