edede13b695ab9d8084911c6c4c46a15c5e66b20
Kurochi51 <andrei5125@gmail.com>
2023-06-14 16:57:26 +0300

    Fix ava updater on Mac

--- src/Ryujinx.Ava/Modules/Updater/Updater.cs
+++ src/Ryujinx.Ava/Modules/Updater/Updater.cs
@@ -740,7 +740,7 @@ namespace Ryujinx.Modules
         {
             var files = Directory.EnumerateFiles(HomeDir); // All files directly in base dir.
 
-            if (_running)
+            if (_running && !OperatingSystem.IsMacOS())
             {
                 //Compare the loose files in base directory against the loose files from the incoming update, and store foreign ones in a user list.
                 var oldFiles = Directory.EnumerateFiles(HomeDir, "*", SearchOption.TopDirectoryOnly).Select(Path.GetFileName);
